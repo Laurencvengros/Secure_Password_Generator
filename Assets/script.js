e16f41eb0d1364274b24d57961950efc2774a860
Lauren <l.cvengros@icloud.com>
2022-08-01 17:32:08 -0400

    fixed password randomizer (working)

--- Assets/script.js
+++ Assets/script.js
@@ -2,11 +2,11 @@
 var generateBtn = document.querySelector("#generate");
 
 
- var charNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8" ,"9"];
- var charUpper = [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
- var charLower = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
- var charSpecial = [ "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-",".","/", ":", ";","<", "=", "?", "@", "[", "^", "_", "`", "{", "]", "|", "}", "~" ];
-
+ var charNumber = ["0123456789"];
+ var charUpper = [ "ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+ var charLower = ["abcdefghijklmnopqrstuvwxyz"];
+ var charSpecial = ["!#$%&'()*+,-./:;<=?@[^_`{]|}~"];
+let randomPassword= "";
 var passwordInclude =[]
 
 // Write password to the #password input
@@ -25,55 +25,57 @@ generateBtn.addEventListener("click", writePassword);
 function generatePassword(){
   var chosenLength =prompt(" Choose the length of your password ( 8-128 characters)"); 
 
-if ((chosenLength <8) || (chosenLength>128)){
-  alert("Password must be between 8 and 128 characters");
-  var chosenLength =prompt(" Choose the length of your password ( 8-128 characters)");
+  if ((chosenLength <8) || (chosenLength>128)){
+    alert("Password must be between 8 and 128 characters");
+    var chosenLength =prompt(" Choose the length of your password ( 8-128 characters)");
 
-}else((chosenLength >= 8) && (chosenLength<= 128)) 
-  console.log( "Length " + chosenLength);
+  }else((chosenLength >= 8) && (chosenLength<= 128)) 
+    console.log( "Length " + chosenLength);
 
 
-var includeLower =confirm ( "Do you want to include lower case letters?");
+  var includeLower =confirm ( "Do you want to include lower case letters?");
 
-if (includeLower){
-  passwordInclude =charLower;
-  console.log(charLower)
-}
+  if (includeLower){
+    passwordInclude += charLower;
+    console.log(charLower)
+  }
 
-var includeUpper =confirm ("Do you want to include upper case letters?");
+  var includeUpper =confirm ("Do you want to include upper case letters?");
 
-if (includeUpper){
-  passwordInclude = charUpper;
-  console.log(charUpper)
-}
+  if (includeUpper){
+    passwordInclude += charUpper;
+    console.log(charUpper)
+  }
 
-var includeNumber =confirm ("Do you want to include numbers?");
+  var includeNumber =confirm ("Do you want to include numbers?");
 
-if (includeNumber){
-  passwordInclude = charNumber;
-  console.log(charNumber)
-}
+  if (includeNumber){
+    passwordInclude += charNumber;
+    console.log(charNumber)
+  }
 
-var includeSpecial =confirm ("Do you want to include special characters?");
+  var includeSpecial =confirm ("Do you want to include special characters?");
 
-if (includeSpecial){
-  passwordInclude = charSpecial;
-  console.log(charSpecial)
+  if (includeSpecial){
+    passwordInclude += charSpecial;
+    console.log(charSpecial)
 
-}else if (!includeLower && !includeUpper && !includeNumber && !includeSpecial){
-  alert("Try again: You must choose parameters")
+  }else if (!includeLower && !includeUpper && !includeNumber && !includeSpecial){
+    alert("Try again: You must choose parameters")
 
-}
+  }
 
-var randomPassword= "";
-
-for (var i=0; i < chosenLength; i++){
-  randomPassword = passwordInclude[Math.floor(Math.random()*passwordInclude.chosenLength)];
   
+  console.log(passwordInclude)
+  for (var i=0; i < chosenLength; i++){
+    console.log(passwordInclude[Math.floor(3)])
+    randomPassword += passwordInclude[Math.floor(Math.random()*passwordInclude.length)];
+    //randomPassword = passwordInclude[Math.floor(Math.random()*passwordInclude.chosenLength)];
+    
 
-}
-
- return randomPassword;
+  }
+  console.log(randomPassword)
+  return randomPassword;
  
 
 }
